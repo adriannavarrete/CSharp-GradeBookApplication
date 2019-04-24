3ea87255ee3b2bc1d9ef63363b41249bfcb9e4db
anavarrete <anavarrete@desertdevco.com>
2019-04-23 21:46:04 -0600

    Fix Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -39,11 +39,10 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
-            else
-            {
-                base.CalculateStudentStatistics(name);
-            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
